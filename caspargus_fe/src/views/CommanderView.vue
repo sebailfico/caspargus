f7c08f3bb2f7550972128c50daafa0af12aaa95d
Sebastiano PERA <spe@vizrt.com>
2025-01-03 13:14:15 +0100

    [ADD] BE launch config; CommanderView live preview

--- caspargus_fe/src/views/CommanderView.vue
+++ caspargus_fe/src/views/CommanderView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { useSocketStore } from "../stores/socketStore";
+import LiveView from "./LiveView.vue";
 
 defineProps<{ msg?: string }>();
 
@@ -11,22 +12,23 @@ function onComponentChange(cmp: string) {
 </script>
 
 <template>
-  <div
-    class="absolute top-0 h-full left-1/2 -translate-x-1/2 w-[50%] elev-4 flenter text-white"
-  >
-    <div>
+  <div class="absolute top-0 left-0 size-full bg-slate-600 text-white pt-4">
+    <div class="relative h-40 w-1/2 left-1/2 -translate-x-1/2">
       COMMANDER <br />
       {{ socketStore.liveComponent }}
 
       <div class="grid grid-cols-2 gap-2">
-        <button class="btn" @click="onComponentChange('L3')">L3</button>
-        <button class="btn" @click="onComponentChange('startingList')">
+        <button class="bg-blue-500" @click="onComponentChange('L3')">L3</button>
+        <button class="bg-blue-500" @click="onComponentChange('startingList')">
           Starting List
         </button>
-        <button class="btn" @click="onComponentChange('')">
+        <button class="bg-blue-500" @click="onComponentChange('')">
           Reset
         </button>
       </div>
+    </div>
+    <div class="relative w-full aspect-video bottom-0">
+      <LiveView />
     </div>
   </div>
 </template>
